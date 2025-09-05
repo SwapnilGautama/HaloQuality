bba755a12a6eddd81fde83a9d5a50b6487c74232
SwapnilGautama <swapnil.gautama@gmail.com>
2025-09-05 12:30:11 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -3,3 +3,8 @@ from .loader_complaints import load_complaints
 from .loader_fpa import load_fpa
 from .fpa_labeller import label_fpa_comments
 from .joiner import build_joined_metrics
+
+# core/__init__.py
+# Mark 'core' as a package and expose modules we use elsewhere.
+__all__ = ["data_store", "ingest", "join_cases_complaints"]
+
