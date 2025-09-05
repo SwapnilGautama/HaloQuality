6bf67667bf2141f1c07bea8c2350eb35fe563caf
SwapnilGautama <swapnil.gautama@gmail.com>
2025-09-05 10:31:16 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,5 @@
 from .loader_cases import load_cases
 from .loader_complaints import load_complaints
+from .loader_fpa import load_fpa
+from .fpa_labeller import label_fpa_comments
 from .joiner import build_joined_metrics
